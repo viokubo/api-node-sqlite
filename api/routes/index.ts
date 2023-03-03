04a119cf701fa865e370a568391ac6c564ce4f26
viokubo <vhideki1996@icloud.com>
2023-03-03 01:22:57 -0300

    exclude vercel configurations

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,11 +1,8 @@
-import { Application, Request, Response, Router } from "express";
+import { Application, Router } from "express";
 import { productRouter } from "./products";
 
 export const useRoutes = (app: Application) => {
   const apiRouter = Router();
   apiRouter.use("/products", productRouter);
   app.use("/api/v1", apiRouter);
-  app.use("/", (_req: Request, res: Response) => {
-    res.json({ message: "ok" });
-  });
 };
